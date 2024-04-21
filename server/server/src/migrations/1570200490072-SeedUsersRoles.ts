fcec594245974d394ddc9ca1646be796b3975c0b
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2024-04-21 15:24:37 +0300

    university seed

--- server/server/src/migrations/1570200490072-SeedUsersRoles.ts
+++ server/server/src/migrations/1570200490072-SeedUsersRoles.ts
@@ -1,7 +1,8 @@
-import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
+import {MigrationInterface, QueryRunner, getRepository, Column} from 'typeorm';
 import { User } from '../domain/user.entity';
 import { transformPassword } from '../security';
 import { Authority } from '../domain/authority.entity';
+import {University} from "../domain/university.entity";
 
 export class SeedUsersRoles1570200490072 implements MigrationInterface {
     role1: Authority = { name: 'ROLE_ADMIN' };
@@ -60,6 +61,20 @@ export class SeedUsersRoles1570200490072 implements MigrationInterface {
         lastModifiedBy: 'system',
     };
 
+  university1: University = {
+    albums: [], comments: [], directions: [], files: [], images: [], products: [],
+    name: 'Крымский инженерно-педагогический университет имени Февзи Якубова',
+    desciption: '',
+    address: '295015, Республика Крым, г. Симферополь, пер. Учебный, д.8',
+    phoneNumber: '+7 (3652) 24-94-95',
+    email: 'cepu@crimeaedu.ru',
+    site: 'https://kipu-rc.ru/',
+    phoneNumbers: '+7 (36561) 6-60-99,+7 (989) 271-43-99',
+    emails: 'cepu-ipk@crimeaedu.ru,cepu-kerch@crimeaedu.ru',
+    schedule: 'Понедельник – пятница: 8:00 – 17:00 (перерыв на обед: 12:00 – 13:00). Режим рабочего времени работников, отнесенных к профессорско-преподавательскому составу, определяется расписанием занятий',
+    responsiblePerson: 'Ибрагимов Таир Шукриевич\t'
+  };
+
     // eslint-disable-next-line
     public async up(queryRunner: QueryRunner): Promise<any> {
         const authorityRepository = getRepository('nhi_authority');
@@ -73,9 +88,12 @@ export class SeedUsersRoles1570200490072 implements MigrationInterface {
         this.user3.authorities = [adminRole, userRole];
         this.user4.authorities = [userRole];
 
+        const universityRepository = getRepository('nhi_university')
+
         await Promise.all([this.user1, this.user2, this.user3, this.user4].map(u => transformPassword(u)));
 
         await userRepository.save([this.user1, this.user2, this.user3, this.user4]);
+        await universityRepository.save([this.university1])
     }
 
     // eslint-disable-next-line
